884ccc49ed8c01de3bca3ebb3a715121b59e5fac
sherlynt <tiwarisherlyn@gmail.com>
2024-08-07 10:10:03 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+//Sherlyn Tiwari
+//Prn: 23070123119
+//experiment 1: calculator 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum=0,diff=0,prod=0,q=0;
+    cout<<"Enter the first no.:";
+    cin>>a;
+    cout<<"Enter the second no.:";
+    cin>>b;
+   
+    sum=a+b;
+    cout<<"Sum of the given nos. is:"<<sum<<endl;
+     diff=a-b;
+    cout<<"Difference of the given nos. is:"<<diff<<endl;
+     prod=a*b;
+    cout<<"Product of the given nos. is:"<<prod<<endl;
+     q=a/b;
+    cout<<"Division of the given nos. is:"<<q<<endl;
+   
+}
